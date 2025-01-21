a9520d6bbd61ee3d597eed5c73f3cce4f6498998
Prince Raj <pr273582@gmail.com>
2025-01-21 21:25:01 +0530

    Add

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
 let btn = document.querySelector("button");
 let div = document.querySelector(".img-container");
 let div2 = document.querySelector(".img-container2");
+let video = document.querySelector("video");
 btn.addEventListener("click", () => {
     btn.innerText = "Let's Dance Now😁";
     let h1 = document.querySelector("body");
     div.style.display = "block";
     div2.style.display = "none";
+    playVideo();
+    video.style.display = "none";
     setInterval( () => {
         h1.style.backgroundColor = `rgb(${Math.floor(Math.random() * 256)},${Math.floor(Math.random() * 256)},${Math.floor(Math.random() * 256)})`;
     },20);
 });
+
+function playVideo() {
+    
+    video.play();
+};
