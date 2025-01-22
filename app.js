e662e3205044885f2bc0ee54f4bf24403e4affbb
Prince Raj <pr273582@gmail.com>
2025-01-22 09:27:31 +0530

    imoproved

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 let btn = document.querySelector("button");
-let div = document.querySelector(".img-container");
-let div2 = document.querySelector(".img-container2");
+let div = document.querySelector(".img1");
+let div2 = document.querySelector("#img2");
 let video = document.querySelector("video");
 btn.addEventListener("click", () => {
     btn.innerText = "Let's Dance Now😁";
     let h1 = document.querySelector("body");
-    div.style.display = "block";
-    div2.style.display = "none";
+    div.style.display = "none";
+    div2.style.display = "block";
     playVideo();
     video.style.display = "none";
     setInterval( () => {
